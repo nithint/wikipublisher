d8994c38d8ede3da3aed6d9f2e6b092b48ec75b1
Nithin Thomas <azianguru@gmail.com>
2011-03-15 08:12:10 -0500

    updating to Jenkins 1.400 updated README

--- wikipublisher/src/main/java/org/jvnet/hudson/plugins/wikipublisher/WikiPublisher.java
+++ wikipublisher/src/main/java/org/jvnet/hudson/plugins/wikipublisher/WikiPublisher.java
@@ -2,19 +2,6 @@
  *
  * FILE: WikiPublisher.java
  *
- * MODULE DESCRIPTION:
- * See class description
- *
- * Copyright (C) 2011 by
- * OCEUSNETWORKS INC
- * USA
- *
- * The program may be used and/or copied only with the written
- * permission from Oceus Networks Inc, or in accordance with
- * the terms and conditions stipulated in the agreement/contract
- * under which the program has been supplied.
- *
- * All rights reserved
  *
  * ============================================================================
  */
